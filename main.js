23dd8039501a40c1b2da3e80af4585b6209fdb9e
Alexander Petriga <cazhqa666@gmail.com>
2021-04-20 18:03:47 +0300

    fixed wrong thisArg feature / fixed filter and forEach methods

--- main.js
+++ main.js
@@ -13,78 +13,72 @@ function MyArray(...args) {
   });
 };
 
-if(!MyArray.prototype.filter) {
-  MyArray.prototype.filter = function (callback, thisArg) {
-    let array = this,
-      result = [],
-      element;
-      thisArg = this;
-      
-      for (let i = 0; i < array.length; i++) {
-        element = array[i];
-        if(callback(element, i, array, thisArg)) result.push(element);
-      }
-      return result;
+MyArray.prototype.filter = function (callback, thisArg) {
+  const array = this;
+        newArray = new MyArray();
+  
+  for (let i = 0; i < array.length; i++) {
+    element = array[i];
+    if(callback.call(thisArg, element, i, array)) {
+      newArray.push(element);
     }
+  }
+  return newArray;
 };
 
-if(!MyArray.prototype.forEach) {
-  MyArray.prototype.forEach = function (callback, thisArg) {
-    let array = this,
-        currentValue;
-  
-    for (let i = 0; i < array.length; i++) {
-      
-      callback(currentValue, i, array, thisArg);
-    }
+MyArray.prototype.forEach = function (callback, thisArg) {
+  let array = this,
+      currentValue;
 
-    return undefined;
+  for (let i = 0; i < array.length; i++) {
+    callback.call(thisArg, currentValue, i, array);
+  }
+
+  return undefined;
+};
+
+MyArray.prototype.map = function (callback, thisArg) {
+  const array = this,
+        newArray = new MyArray();
+
+  for (let i = 0; i < array.length; i++) {
+    currentValue = array[i];
+    newArray[i] = callback.call(thisArg, array[i], currentValue, array);
+  }
+  return newArray;
+};
+
+MyArray.prototype.push = function(...args) {
+  if(args) {
+    for (let i = 0; i < args.length; i++) {
+      this[this.length] = args[i];
+    }
+    return this.length;
   }
 };
 
-if(!MyArray.prototype.map) {
-  MyArray.prototype.map = function (callback, thisArg) {
-    const array = this,
-          newArray = new MyArray();
 
-    for (let i = 0; i < array.length; i++) {
-      currentValue = array[i];
-      newArray[i] = callback.call(thisArg, array[i], currentValue, array);
-    }
-    return newArray;
-  };
-}
-
-
-
-if (!MyArray.prototype.push) {
-  MyArray.prototype.push = function(...args) {
-    if(args) {
-      for (let i = 0; i < args.length; i++) {
-        this[this.length] = args[i];
-      }
-      return this.length;
-    }
-  };
-};
-
-if (!MyArray.prototype.pop) {
-  MyArray.prototype.pop = function() {
-    const latest = this[this.length - 1];
-    delete this[this.length - 1];
-    return latest;
-  };
+MyArray.prototype.pop = function() {
+  const latest = this[this.length - 1];
+  delete this[this.length - 1];
+  return latest;
 };
 
 
 //test filter method
 //console.log(arr.filter(item => item % 2 === 0))
+// console.log(arr.filter(
+  // function (item) {
+  //   return item % 2 === 0
+  // }))
 
 //test forEach method
-//console.log(arr.forEach(item => console.log(item)))
+// console.log(arr.forEach(function (item, i , arr) {
+//   console.log(item, i , arr)
+// }), {a: 1, b: 2})
 
 //test map method
-console.log(arr.map(item => item * 2))
+//console.log(arr.map(item => item * 2))
 
 //test push method
 //console.log(arr.push(12, 120, 4341, "rofl"));
